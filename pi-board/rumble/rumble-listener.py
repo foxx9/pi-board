702bdaa04d20d850812cdbdd648285c4722c2d4d
Maxime LAYAT <maxime.layat@gmail.com>
2020-06-05 12:07:39 +0200

    rumble is sending led keys correctly

--- pi-board/rumble/rumble-listener.py
+++ pi-board/rumble/rumble-listener.py
@@ -3,25 +3,38 @@ import fcntl
 import os
 import signal
 import struct
+import time
 
-event_path = '/dev/input/event1'
+event = os.popen('cat /proc/bus/input/devices | grep haptic -A 10 | grep Handlers |  cut -f2 -d"="').read()
+
+if not event:
+    print("No haptic device found")
+    exit()
+
+event_path = '/dev/input/' + event.rstrip().strip()
+
+print("Will listen on " + event_path)
+
 FORMAT = 'llHHI'
 EVENT_SIZE = struct.calcsize(FORMAT)
 
 EV_FF = 21
+EV_SYN = 0
 
-FORCE_EMPTY = 0
-FORCE_LOW = 1
-FORCE_MIDDLE = 2
-FORCE_HIGH = 4
+FORCE_EMPTY = 0x00
+FORCE_LOW = 0x01
+FORCE_MIDDLE = 0x02
+FORCE_HIGH = 0x04
 
 KDSETLED = 0x4B32
 LOOP = True
 
 
-def graceful_quit():
+def graceful_quit(signum, frame):
     global LOOP
     LOOP = False
+    time.sleep(1)
+    raise OSError("Force stop reading open files")
 
 
 signal.signal(signal.SIGINT, graceful_quit)
@@ -34,7 +47,7 @@ def send_led_state(led):
 
 def map_code(c):
     if c == 0:
-        return 0
+        return FORCE_EMPTY
     elif c < 7:
         return FORCE_LOW
     elif c < 15:
@@ -44,15 +57,24 @@ def map_code(c):
 
 
 def handle_code(c):
-    force = map_code(c)
-    if force is not FORCE_EMPTY:
-        send_led_state(force)
+    if c != 0:
+        send_led_state(c)
 
 
-with os.open('/dev/console', os.O_NOCTTY) as console_fd:
-    with open(event_path, "rb") as in_file:
+console_fd = os.open('/dev/console', os.O_NOCTTY)
+current_code = 0
+with open(event_path, "rb") as in_file:
+    event = in_file.read(EVENT_SIZE)
+    while LOOP and event:
+        (tv_sec, tv_usec, e_type, code, value) = struct.unpack(FORMAT, event)
+        if e_type == EV_FF and value == 1:
+            # we stack events of the same SYN REPORT
+            current_code = current_code | map_code(code)
+        elif e_type == EV_SYN:
+            # print('should send' + str(current_code))
+            handle_code(current_code)
+            current_code = 0
         event = in_file.read(EVENT_SIZE)
-        while LOOP and event:
-            (tv_sec, tv_usec, e_type, code, value) = struct.unpack(FORMAT, event)
-            if e_type == EV_FF and value == 1:
-                handle_code(code)
+
+os.close(console_fd)
+print("Stopped")
